13255f757b161c272e7b47e4dd1ce065c220737a
asmita-walimbe <146116578+asmita-walimbe@users.noreply.github.com>
2023-10-10 21:18:08 +0530

    changed file name

---
+++ TDD-IntegrationTests/HealthCheckTest.cs
@@ -0,0 +1,21 @@
+﻿namespace TDD_IntegrationTests
+{
+    public class HealthCheckTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public HealthCheckTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task HealthCheckApi_Returns_Healthy_Response()
+        {
+            var response = await _client.GetAsync(Constants.HealthCheckApi);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
