fe236097cbd5e3f535fb51646a7b411d1e573682
asmita-walimbe <146116578+asmita-walimbe@users.noreply.github.com>
2023-10-17 21:01:54 +0530

    Added APIs with Mongo DB integration

--- TDDProject/Interfaces/IUserService.cs
+++ TDDProject/Interfaces/IUserService.cs
@@ -4,7 +4,10 @@ namespace TDDProject.Interfaces
 {
     public interface IUserService
     {
-        public Task<User> GetByIdAsync(int userId);
-        public Task<User> AddUserAsync(User user);
+        public Task<User> GetByIdAsync(int Id);
+        public Task<List<User>> GetAllAsync();
+        public Task<User> AddAsync(User user);
+        public Task UpdateAsync(int id, User user);
+        public Task DeleteAsync(int id);
     }
 }
--- TDDProject/Services/UserService.cs
+++ TDDProject/Services/UserService.cs
@@ -1,24 +1,49 @@
-﻿using TDDProject.Interfaces;
+﻿using MongoDB.Driver;
+using TDDProject.Interfaces;
 using TDDProject.Models;
+using TDDProject.MongoDB;
 
 namespace TDDProject.Services
 {
     public class UserService : IUserService
     {
-        public async Task<User> AddUserAsync(User user)
+        private readonly MongoDBContext _context;
+        private readonly IMongoCollection<User> _usersCollection;
+
+        public UserService(MongoDBContext context)
         {
-            user.Id = 1;
+            _context = context;
+            _usersCollection = _context._collections;
+        }
+        public async Task<User> AddAsync(User user)
+        {
+            Random getrandom = new Random();
+            user.UserId = getrandom.Next(1, 200);
+            await _usersCollection.InsertOneAsync(user);
             return user;
         }
 
+        public async Task UpdateAsync(int id, User updatedUser)
+        {
+            var filter = Builders<User>.Filter
+                .Eq(user => user.UserId, updatedUser.UserId);
+            var update = Builders<User>.Update
+                .Set(user => user.Name, updatedUser.Name)
+                .Set(user => user.Address, updatedUser.Address);
+
+            await _usersCollection.UpdateOneAsync(filter, update);
+        }
+
+        public async Task<List<User>> GetAllAsync() =>
+        await _usersCollection.Find(_ => true).ToListAsync();
+
         public async Task<User> GetByIdAsync(int userId)
         {
-            // will get the data from db later.
-            var users = new List<User>();
-            users.Add(new User(1, "Test", "Pune"));
-            users.Add(new User(2, "Test2", "Chennai"));
-            var response = users.FirstOrDefault(x => x.Id == userId);
+            var response = await _usersCollection.Find(x => x.UserId == userId).FirstOrDefaultAsync();
             return response;
         }
+
+        public async Task DeleteAsync(int id) =>
+            await _usersCollection.DeleteOneAsync(x => x.UserId == id);
     }
 }
